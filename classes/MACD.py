2ac16566de9649cb2761f3b157dd3022eee5c099
Petar Kelava <pkelava@mathos.hr>
2021-07-28 16:27:51 +0200

    Basics of interface

--- classes/MACD.py
+++ classes/MACD.py
@@ -3,19 +3,19 @@ from classes.BaseTactic import BaseTactic
 class MACD(BaseTactic):
     def __init__(self, data, period):
         BaseTactic.__init__(self, data, period)
+        self.buySignals = []
         self.EMA12 = []
         self.EMA26 = []
-        self.subtracted = []
+        self.MACDline = []
 
-
-    def CalculateMovingAverage(self):
+    def CalculateMovingAverage(self, movingAverage, data = None):
         self.ChangePeriod(12)
-        BaseTactic.CalculateMovingAverage(self, "EMA")
-        self.EMA12 = self.ExponentialMovingAverage
+        self.EMA12 = BaseTactic.CalculateMovingAverage(self, "EMA", data)
 
         self.ClearMovingAverages()
         self.ChangePeriod(26)
-        BaseTactic.CalculateMovingAverage(self, "EMA")
-        self.EMA26 = self.ExponentialMovingAverage
+        self.EMA26 = BaseTactic.CalculateMovingAverage(self, "EMA", data)
 
-        self.subtracted = list(map(lambda x, y: x - y, self.EMA12, self.EMA26))
+        self.MACDline = list(map(lambda x, y: round(x - y,4), self.EMA12, self.EMA26))
+        self.ChangePeriod(9)
+        self.signalLine = BaseTactic.CalculateMovingAverage(self, "EMA", self.MACDline)
