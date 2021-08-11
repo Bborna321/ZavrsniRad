f8396da75cbd28a37de5982dc325b5e5a00d47cf
Petar Kelava <pkelava@mathos.hr>
2021-08-11 17:25:03 +0200

    Git bugged out

--- global_vars.py
+++ global_vars.py
@@ -1,18 +1,17 @@
 from matplotlib import style
 from matplotlib.figure import Figure
 
-
-large_font=('Verdana',12)
-normal_font=('Verdana',10)
-small_font=('Verdana',8)
+large_font = ('Verdana', 12)
+normal_font = ('Verdana', 10)
+small_font = ('Verdana', 8)
 style.use('ggplot')
 
 darkColor = "#892020"
 lightColor = "#208989"
 lightColor_prev = "#7fff00"
 
-f = Figure(figsize=(21,9), dpi=90)
-#f = Figure()
+f = Figure(figsize=(21, 9), dpi=90)
+# f = Figure()
 a = f.add_subplot(111)
 
 exchange = "BTC-e"
@@ -20,16 +19,16 @@ DatCounter = 9000
 programName = "coinbase"
 
 resampleSize = "15Min"
-dataPace="tick"
+dataPace = "tick"
 candleWidth = 0.008
-paneCount=1
+paneCount = 1
 
-topIndicator="none"
-botIndicator="none"
-midIndicator="none"
-EMAs=[]
-SMAs=[]
-chartLoad=True
+topIndicator = "none"
+botIndicator = "none"
+midIndicator = "none"
+EMAs = []
+SMAs = []
+chartLoad = True
 
-market = "LTC-EUR"
 granularity = 86400
+
